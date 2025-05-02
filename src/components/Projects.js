4b18d18c72377121961b62e55997cd854ff597a4
sanjithag <sanjithg@andrew.cmu.edu>
2025-05-02 00:28:50 -0400

    projects page push 1

---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => (
+  <section id="experience" className="p-9">
+    <h2>Projects</h2>
+    <p>Sprout Coffee (Python)</p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/LYrePuiwzhU?si=-MM1oUCjf_ZZ0klw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </section>
+);
+
+export default Experience;
