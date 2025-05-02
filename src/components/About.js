329fae5492a981c51c783b0b09c9136a0fe68516
sanjithag <sanjithg@andrew.cmu.edu>
2025-05-02 00:21:17 -0400

    about page push 1

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about" className="p-5">
+    <h2>About Me</h2>
+    <p>Hi, I'm Sanjitha, a future product manager currently studying Information Systems at Carnegie Mellon University.</p>
+  </section>
+);
+
+export default About;
