ef61939787c8ac952a66f6166b7324ccdd3f1b5e
sanjithag <sanjithg@andrew.cmu.edu>
2025-05-02 00:21:29 -0400

    contact page push 1

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact" className="p-6">
+    <h2>Contact</h2>
+    <p>Phone Number: (916) 385-9173</p>
+    <p><a href='sanjithg@andrew.cmu.edu'>Email: sanjithg@andrew.cmu.edu</a></p>
+  </section>
+);
+
+export default Contact;
