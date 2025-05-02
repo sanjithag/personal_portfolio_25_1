59bffc4aef13fede8d3ba73ba7a7a46d9f2541b4
sanjithag <sanjithg@andrew.cmu.edu>
2025-05-02 00:24:19 -0400

    education page push 1

---
+++ src/components/Education.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Education = () => (
+  <section id="education" className="p-7">
+    <h2>Education</h2>
+    <h3>Carnegie Mellon University</h3>
+    <p>Major: B.S. Information Systems</p>
+    <p>Minor: Business Administration</p>
+    <p>Minor: Artificial Intelligence</p>
+    <br></br>
+    <p>
+        Relevant Coursework: 
+        15-112 Fundamentals of Programming & Computer Science,
+        15-110 Principles of Computing,
+        67-250 Information Systems Milieux,
+        70-100 Global Business,
+        73-102 Principles of Microeconomics
+        </p>
+
+  </section>
+);
+
+export default Education;
