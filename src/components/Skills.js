3045cea4d3c60cafbac662b5d9b0ae52ad99879b
sanjithag <sanjithg@andrew.cmu.edu>
2025-05-02 00:33:27 -0400

    skills page push 1

---
+++ src/components/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skills = () => (
+  <section id="skills" className="p-10">
+    <h2>Skills</h2>
+    <p>Technical Skills: Python, SQL, Javascript, HTML/CSS, Power BI, MS Office</p>
+    <p>Professional Skills: Leadership, Collaboration, Time Management</p>
+  </section>
+);
+
+export default Projects;
