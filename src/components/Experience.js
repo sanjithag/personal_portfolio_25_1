05ebe7c11bbd8390714d2a35de1242f7beb42f08
sanjithag <sanjithg@andrew.cmu.edu>
2025-05-02 00:25:58 -0400

    experience page push 1

---
+++ src/components/Experience.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Experience = () => (
+  <section id="experience" className="p-8">
+    <h2>Experience</h2>
+    <p>End User Experience Intern at SMUD (Sacramento Municipal Utility District)</p>
+    <br></br>
+    <p>Information Technology Intern at SMUD (Sacramento Municipal Utility District)</p>
+  </section>
+);
+
+export default Experience;
